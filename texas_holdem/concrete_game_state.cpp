c2fd7e8eca9592e34e7095ce12e36faa7d4d22d3
jerrywang1010 <40965554+jerrywang1010@users.noreply.github.com>
2022-01-11 11:05:48 -0500

    Update concrete_game_state.cpp

--- texas_holdem/concrete_game_state.cpp
+++ texas_holdem/concrete_game_state.cpp
@@ -1,7 +1,6 @@
 #include "concrete_game_state.h"
 #include <iostream>
 
-
 /****************** Pre flop round ******************/
 // Ask the user for an Action
 // check if it's legal
@@ -19,7 +18,9 @@ void Pre_flop::process(Game* game)
         Player& current_player = game->m_players[i];
         // assign cards[0, 1] to player 0's hand
         // assign cards[2, 3] to player 1's hand
-        current_player.add_to_hand( {game->m_cards[2 * i], game->m_cards[2 * i + 1]} );
+        Hand player_hand = {game->m_cards[2 * i], game->m_cards[2 * i + 1]};
+        current_player.add_to_hand(player_hand);
+        current_player.add_to_usable_cards(player_hand);
         current_player.display_hand();
 
         // don't allow check
@@ -45,14 +46,14 @@ void Pre_flop::process(Game* game)
 void Pre_flop::enter(Game* game)
 {
     std::cout << "***************************************************************" << std::endl;
-    std::cout << "Entering Pre Flop Round:\t" << "pot=" << game->m_pot << std::endl;
-    std::cout << game->m_players[0].player_name << "'s balance=" << game->m_players[0].get_balance() << std::endl;
-    std::cout << game->m_players[1].player_name << "'s balance=" << game->m_players[1].get_balance() << std::endl;
+    std::cout << GREEN << "Entering Pre Flop Round:\t" << "pot=" << game->m_pot << RESET << std::endl;
+    std::cout << YELLOW << game->m_players[0].player_name << "'s balance=" << game->m_players[0].get_balance() << RESET << std::endl;
+    std::cout << YELLOW << game->m_players[1].player_name << "'s balance=" << game->m_players[1].get_balance() << RESET << std::endl;
 }
 
 void Pre_flop::exit(Game* game)
 {
-    std::cout << "Exiting Pre Flop Round\t" << "pot=" << game->m_pot << std::endl << std::endl;
+    std::cout << CYAN << "Exiting Pre Flop Round\t" << "pot=" << game->m_pot << RESET << std::endl << std::endl;
     // update history action
     game->update_his_action();
     // clear this round action
@@ -109,20 +110,22 @@ void Flop::enter(Game* game)
     // reset the flag because it's a new round
     check_allowed = true;
     std::cout << "***************************************************************" << std::endl;
-    std::cout << "Entering Flop Round:\t" << "pot=" << game->m_pot << std::endl;
-    std::cout << game->m_players[0].player_name << "'s balance=" << game->m_players[0].get_balance() << std::endl;
-    std::cout << game->m_players[1].player_name << "'s balance=" << game->m_players[1].get_balance() << std::endl;
+    std::cout << GREEN << "Entering Flop Round:\t" << "pot=" << game->m_pot << RESET << std::endl;
+    std::cout << YELLOW << game->m_players[0].player_name << "'s balance=" << game->m_players[0].get_balance() << RESET << std::endl;
+    std::cout << YELLOW << game->m_players[1].player_name << "'s balance=" << game->m_players[1].get_balance() << RESET << std::endl;
 
     // add 3 more community card to player's hand, ie cards[4, 5, 6]
     for (auto& player : game->m_players)
     {
-        player.add_to_hand( {game->m_cards[4], game->m_cards[5], game->m_cards[6]} );
+        player.add_to_usable_cards( {game->m_cards[4], game->m_cards[5], game->m_cards[6]} );
     }
+    game->add_to_community_cards( {game->m_cards[4], game->m_cards[5], game->m_cards[6]} );
+    game->display_community_cards();
 }
 
 void Flop::exit(Game* game)
 {
-    std::cout << "Exiting Flop Round\t" << "pot=" << game->m_pot << std::endl << std::endl;
+    std::cout << CYAN << "Exiting Flop Round\t" << "pot=" << game->m_pot << RESET << std::endl << std::endl;
     game->update_his_action();
     game->clear_round_action();
 }
@@ -177,20 +180,22 @@ void Turn::enter(Game* game)
     // reset the flag because it's a new round
     check_allowed = true;
     std::cout << "***************************************************************" << std::endl;
-    std::cout << "Entering Turn Round:\t" << "pot=" << game->m_pot << std::endl << std::endl;
-    std::cout << game->m_players[0].player_name << "'s balance=" << game->m_players[0].get_balance() << std::endl;
-    std::cout << game->m_players[1].player_name << "'s balance=" << game->m_players[1].get_balance() << std::endl;
+    std::cout << GREEN << "Entering Turn Round:\t" << "pot=" << game->m_pot << RESET << std::endl << std::endl;
+    std::cout << YELLOW << game->m_players[0].player_name << "'s balance=" << game->m_players[0].get_balance() << RESET << std::endl;
+    std::cout << YELLOW << game->m_players[1].player_name << "'s balance=" << game->m_players[1].get_balance() << RESET << std::endl;
 
     // add 1 more community card to player's hand, ie cards[7]
     for (auto& player : game->m_players)
     {
-        player.add_to_hand( {game->m_cards[7]} );
+        player.add_to_usable_cards( {game->m_cards[7]} );
     }
+    game->add_to_community_cards( {game->m_cards[7]} );
+    game->display_community_cards();
 }
 
 void Turn::exit(Game* game)
 {
-    std::cout << "Exiting Turn Round\t" << "pot=" << game->m_pot << std::endl;
+    std::cout << CYAN << "Exiting Turn Round\t" << "pot=" << game->m_pot << RESET << std::endl;
     game->update_his_action();
     game->clear_round_action();
 }
@@ -244,20 +249,22 @@ void River::enter(Game* game)
     // reset the flag because it's a new round
     check_allowed = true;
     std::cout << "***************************************************************" << std::endl;
-    std::cout << "Entering River Round:\t" << "pot=" << game->m_pot << std::endl << std::endl;
-    std::cout << game->m_players[0].player_name << "'s balance=" << game->m_players[0].get_balance() << std::endl;
-    std::cout << game->m_players[1].player_name << "'s balance=" << game->m_players[1].get_balance() << std::endl;
+    std::cout << GREEN << "Entering River Round:\t" << "pot=" << game->m_pot << RESET << std::endl << std::endl;
+    std::cout << YELLOW << game->m_players[0].player_name << "'s balance=" << game->m_players[0].get_balance() << RESET << std::endl;
+    std::cout << YELLOW << game->m_players[1].player_name << "'s balance=" << game->m_players[1].get_balance() << RESET << std::endl;
 
     // show last community card, add to player's hand
     for (auto& player : game->m_players)
     {
-        player.add_to_hand( {game->m_cards[8]} );
+        player.add_to_usable_cards( {game->m_cards[8]} );
     }
+    game->add_to_community_cards( {game->m_cards[8]} );
+    game->display_community_cards();
 }
 
 void River::exit(Game* game)
 {
-    std::cout << "Exiting River Round\t" << "pot=" << game->m_pot << std::endl;
+    std::cout << CYAN << "Exiting River Round\t" << "pot=" << game->m_pot << RESET << std::endl;
     game->update_his_action();
     game->clear_round_action();
 }
@@ -274,22 +281,22 @@ void Showdown::process(Game* game)
 {
     // evaluate the hand of each player and determine who won
 
-    uint16_t player_0_score = game->get_hand_ranking(game->m_players[0].get_hand());
-    uint16_t player_1_score = game->get_hand_ranking(game->m_players[1].get_hand());
+    uint16_t player_0_score = game->get_hand_ranking(game->m_players[0].get_usable_cards());
+    uint16_t player_1_score = game->get_hand_ranking(game->m_players[1].get_usable_cards());
 
     if (player_0_score > player_1_score)
     {
-        std::cout << game->m_players[0].player_name << " won the game, winning $" << game->m_pot << std::endl;
+        std::cout << RED << game->m_players[0].player_name << " won the game, winning $" << game->m_pot << RESET << std::endl;
         game->m_loss_player_idx = 1;
     }
     else if (player_0_score < player_1_score)
     {
-        std::cout << game->m_players[1].player_name << " won the game, winning $" << game->m_pot << std::endl;
+        std::cout << RED << game->m_players[1].player_name << " won the game, winning $" << game->m_pot << RESET << std::endl;
         game->m_loss_player_idx = 0;
     }
     else
     {
-        std::cout << "Game is tie" << std::endl;
+        std::cout << RED << "Game is tie" << RESET << std::endl;
     }
     game->transition(Terminal::get_instance());
 }
@@ -297,14 +304,14 @@ void Showdown::process(Game* game)
 void Showdown::enter(Game* game)
 {
     std::cout << "***************************************************************" << std::endl;
-    std::cout << "Entering Showdown Round:\t" << "pot=" << game->m_pot << std::endl;
-    std::cout << game->m_players[0].player_name << "'s balance=" << game->m_players[0].get_balance() << std::endl;
-    std::cout << game->m_players[1].player_name << "'s balance=" << game->m_players[1].get_balance() << std::endl;
+    std::cout << GREEN << "Entering Showdown Round:\t" << "pot=" << game->m_pot << RESET << std::endl;
+    std::cout << YELLOW << game->m_players[0].player_name << "'s balance=" << game->m_players[0].get_balance() << RESET << std::endl;
+    std::cout << YELLOW << game->m_players[1].player_name << "'s balance=" << game->m_players[1].get_balance() << RESET << std::endl;
 }
 
 void Showdown::exit(Game* game)
 {
-    std::cout << "Exiting Showdown Round\t" << "pot=" << game->m_pot << std::endl << std::endl;
+    std::cout << CYAN << "Exiting Showdown Round\t" << "pot=" << game->m_pot << RESET << std::endl << std::endl;
 }
 
 
@@ -313,9 +320,6 @@ Game_state& Showdown::get_instance()
     static Showdown singleton;
     return singleton;
 }
-
-
-// 11s 13d, 4c 1c, 4s 12h 12d
 
 
 /****************** Terminal state ********************/
@@ -328,7 +332,9 @@ void Terminal::process(Game* game)
     // clear player action history
     unsigned winning_palyer_idx = game->m_loss_player_idx ^ 1;
 
-    std::cout << game->m_players[winning_palyer_idx].player_name << " won the game, winning $" << game->m_pot << std::endl;
+    // std::cout << game->m_players[winning_palyer_idx].player_name << " won the game, winning $" << game->m_pot << std::endl;
+
+    game->display_round_result();
     
     game->m_players[winning_palyer_idx].update_balanace(game->m_pot);
 
@@ -342,6 +348,10 @@ void Terminal::process(Game* game)
 
     game->clear_player_hand();
 
+    game->clear_player_usable_cards();
+
+    game->clear_community_cards();
+
     game->m_cards.clear();
 
     // continue if we haven't reached the round limit
@@ -354,14 +364,14 @@ void Terminal::process(Game* game)
 void Terminal::enter(Game* game)
 {
     std::cout << "***************************************************************" << std::endl;
-    std::cout << "Entering Terminal Round:\t" << "pot=" << game->m_pot << std::endl;
-    std::cout << game->m_players[0].player_name << "'s balance=" << game->m_players[0].get_balance() << std::endl;
-    std::cout << game->m_players[1].player_name << "'s balance=" << game->m_players[1].get_balance() << std::endl;
+    std::cout << GREEN << "Entering Terminal Round:\t" << "pot=" << game->m_pot << RESET << std::endl;
+    std::cout << YELLOW << game->m_players[0].player_name << "'s balance=" << game->m_players[0].get_balance() << RESET << std::endl;
+    std::cout << YELLOW << game->m_players[1].player_name << "'s balance=" << game->m_players[1].get_balance() << RESET << std::endl;
 }
 
 void Terminal::exit(Game* game)
 {
-    std::cout << "Exiting Terminal Round\t" << "pot=" << game->m_pot << std::endl << std::endl;
+    std::cout << CYAN << "Exiting Terminal Round\t" << "pot=" << game->m_pot << RESET << std::endl << std::endl;
 }
 
 Game_state& Terminal::get_instance()
