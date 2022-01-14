69d7cf4b1a7f8fa4ee933f6e219d528810393401
jerrywang1010 <40965554+jerrywang1010@users.noreply.github.com>
2022-01-13 22:19:03 -0500

    Update main.cpp

--- texas_holdem/main.cpp
+++ texas_holdem/main.cpp
@@ -4,14 +4,21 @@
 #include <cstdio>
 // #include "game_rule.h"
 #include "game.h"
+#include "gametree.h"
 
 int main()
 {
     // Set console code page to UTF-8 so console known how to interpret string data
-    SetConsoleOutputCP(CP_UTF8);
+    //SetConsoleOutputCP(CP_UTF8);
 
-    Game game;
-    game.run(10);
+    //Game game;
+    //game.run(10);
+
+    Hand deck = {1,2,3,4,5,6,7,8,9,10,11,12,13};
+    GameTree tree;
+    tree.build_tree(deck);
+
+    
 
     // Enable buffering to prevent VS from chopping up UTF-8 byte sequences
     // setvbuf(stdout, nullptr, _IOFBF, 1000);
