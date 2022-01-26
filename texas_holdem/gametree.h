62b9998b6a372b9071138a9868bda1ed4d5a9352
jerrywang1010 <40965554+jerrywang1010@users.noreply.github.com>
2022-01-26 16:55:58 -0500

    Update gametree.h

--- texas_holdem/gametree.h
+++ texas_holdem/gametree.h
@@ -13,7 +13,7 @@ public:
     GameTree() : root(nullptr), m_eval (omp::HandEvaluator()) {}
     void build_tree(Hand deck);
     void recursive_build_tree(TreeNode* parent, Board_state args);
-    void print_tree();
+    void print_tree(std::ostream& s = std::cout);
     Node_type get_next_node_type(TreeNode* parent, Action a);
     std::vector<Action> get_legal_actions(TreeNode* parent);
 };
