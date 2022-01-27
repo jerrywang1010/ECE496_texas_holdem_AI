fc93127a88d2d4e39c02f4502371dfc4d5e8edc6
jerrywang1010 <40965554+jerrywang1010@users.noreply.github.com>
2022-01-27 14:24:34 -0500

    Update treenode.h

--- texas_holdem/treenode.h
+++ texas_holdem/treenode.h
@@ -11,15 +11,40 @@
 
 typedef struct Infoset
 {
-    std::vector<Action> action_history;
-    Hand private_card;
-    Hand community_card;
-    std::pair<int, int> committed;
+    // use 32 bit to represent action history, 2 bits per action, lower bits are more recent
+    // default to be all Action::INVALID 
+    uint32_t action_history = 0;
 
-    void add_to_action_history(Action a) {action_history.push_back(a);}
-    void add_to_private_card(Hand h) {private_card.insert(private_card.end(), h.begin(), h.end());}
-    void add_to_community_card(Hand h) {community_card.insert(community_card.end(), h.begin(), h.end());}
-    void commit(int player_idx, int bet) {
+    // use 32 bit to represent upto 5 community cards, each card is 6 bits ranging from 0x0 to 0x33
+    uint32_t community_card = 0;
+
+    // use 16 bit to represent 2 private cards, each card is 6 bits ranging from 0x0 to 0x33
+    uint16_t private_card = 0;
+
+    // committed can not exceed 255
+    std::pair<uint8_t, uint8_t> committed = {0, 0};
+
+    // round_idx ranging from 0 to 3
+    uint8_t round_idx = 0;
+
+    void add_to_action_history(Action a)
+    {
+        assert(a != Action::INVALID);
+        ADD_TO_ACTIONS(action_history, a);
+    }
+
+    void add_to_private_card(Hand h) 
+    {
+        for (Card c : h) {ADD_TO_HAND(private_card, c);}
+    }
+
+    void add_to_community_card(Hand h) 
+    {
+        for (Card c : h) {ADD_TO_HAND(community_card, c);}
+    }
+
+    void commit(int player_idx, int bet) 
+    {
         if(player_idx == 0)
         {
             committed.first += bet;
@@ -29,35 +54,46 @@ typedef struct Infoset
             committed.second += bet;
         }
     }
-    
+
     void print_infoset(std::ostream& s)
     {
         s << "========INFORMATION SET========" << "\n";
         if (action_history != 0)
         {
             s << "Action_history: ";
-            UTILS::print_action_vec(action_history, s);
+            UTILS::print_action_history(action_history, s);
         }
 
-        if (private_card.size())
+        if (private_card != 0)
         {
+            // 0xCF2 is the largest possible private card = {51, 50} in binary
+            assert(private_card >= 0 && private_card <= 0xCF2);
             s << "Private_card: ";
-            UTILS::print_hand(private_card, s);
+            UTILS::print_hand<uint16_t>(private_card, s, 2);
         }
-        if (community_card.size())
+        if (community_card != 0)
         {
+            // 0x33CB1C2F is the largest possible community card = {51, 50, 49, 48, 47} in binary
+            assert(community_card >= 0 && community_card <= 0x33CB1C2F);
             s << "Community_card: ";
-            UTILS::print_hand(community_card, s);
+            // flop
+            if (round_idx == 1) UTILS::print_hand<uint32_t>(community_card, s, 3);
+            // turn
+            else if (round_idx == 2) UTILS::print_hand<uint32_t>(community_card, s, 4);
+            // river
+            else if (round_idx == 3) UTILS::print_hand<uint32_t>(community_card, s, 5);
+            else fprintf(stderr, "round_idx=%d in print_infoset\n", round_idx);
         }
 
-        s << "Committed: [" << committed.first << ", " << committed.second << "]" << "\n";
+        s << "Committed: [" << static_cast<int>(committed.first) << ", " << static_cast<int>(committed.second) << "]" << "\n";
 
         s << "===============================" << "\n";
     }
-
 } Infoset;
 
 
+// this is okay to have bad memory performace, because it is used as an intermediate argument to pass around functions,
+// it will not stay in the tree permanately like everthing else in this file.
 typedef struct Board_state
 {
     Hand private_card;
@@ -82,7 +118,6 @@ public:
     TreeNode* parent = nullptr;
     std::vector<TreeNode*> children;
     Infoset infoset;
-    int round_idx = 0;
     bool is_terminal = false;
     bool is_chance = false;
     virtual void print_node(int depth, std::ostream& s) {};
@@ -105,7 +140,8 @@ public:
 class ActionNode : public TreeNode
 {
 private:
-    std::vector<Action> action_this_round;
+    // at most 3 actions per round in cases of check, bet, bet
+    uint8_t action_this_round = 0;
     friend class GameTree;
 
 public:
