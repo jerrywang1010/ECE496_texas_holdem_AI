0a1a3bb5f9f5599bcf4d5ea32af0b978fa0881c1
jerrywang1010 <40965554+jerrywang1010@users.noreply.github.com>
2022-02-06 23:34:34 -0500

    Update gametree.h

--- texas_holdem/gametree.h
+++ texas_holdem/gametree.h
@@ -14,8 +14,8 @@ public:
     std::unordered_map<int, std::vector<TreeNode*> > tree_with_depth;
     GameTree() : root(nullptr), m_eval (omp::HandEvaluator()) {}
     void build_tree(Hand deck);
-    void recursive_build_tree(TreeNode* parent, Board_state args);
+    void recursive_build_tree(TreeNode* parent, Build_node_args args);
     void print_tree(std::ostream& s = std::cout);
-    Node_type get_next_node_type(TreeNode* parent, Action a);
-    std::vector<Action> get_legal_actions(TreeNode* parent);
+    inline Node_type get_next_node_type(TreeNode* parent, Action a, int round_idx, const std::vector<Action>& action_this_round);
+    inline std::vector<Action> get_legal_actions(TreeNode* parent, int round_idx, const std::vector<Action> & action_this_round);
 };
