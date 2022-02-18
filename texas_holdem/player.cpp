39785ea602247dc78f4e189c0a8f438ba8b2a3e1
jerrywang1010 <40965554+jerrywang1010@users.noreply.github.com>
2022-02-17 23:31:32 -0500

    Update player.cpp

--- texas_holdem/player.cpp
+++ texas_holdem/player.cpp
@@ -2,63 +2,66 @@
 #include "assert.h"
 #include <algorithm>
 
+
 /**
  * The function updates the balance of a player, if he wins or losses money
  *
  * @param  int
  * @return void 
  */
-void Player::update_balanace(int money)
-{
-    this->m_balance += money;
-}
+// void Player::update_balanace(int money)
+// {
+//     this->m_balance += money;
+// }
 
 /**
- * The function adds card to a player's hand, representing avaliable cards that he could use
+ * The function adds card to a player's private_cards, representing avaliable cards that he could use
  *
  * @param  Hand
  * @return void 
  */
-void Player::add_to_hand(Hand h)
-{
-    assert(h.size() == 2);
-    this->m_hand.insert(m_hand.end(), h.begin(), h.end());
-}
+// void Player::add_to_private_cards(Hand h)
+// {
+//     assert(h.size() == 2);
+//     this->private_cards.insert(this->private_cards.end(), h.begin(), h.end());
+// }
+
 
 /**
- * The function adds a hand to the players usable cards
+ * The function adds a hand to the players community cards
  *
  * @param  hand
  * @return void 
  */
-void Player::add_to_usable_cards(Hand h)
-{
-    assert(h.size() <= 3);
-    this->m_usable_cards.insert(m_usable_cards.end(), h.begin(), h.end());
-}
+// void Player::add_to_community_cards(Hand h)
+// {
+//     assert(h.size() <= 3);
+//     this->community_cards.insert(community_cards.end(), h.begin(), h.end());
+// }
 
 
 /**
- * The function clears the hand of a player
- *
- * @param void
- * @return void 
- */
-void Player::clear_hand()
-{
-    this->m_hand.clear();
-}
-
-/**
- * The functions clears the player's usable cards, usually happends at the end of a game.
+ * The functions clears the player's private cards, usually happends at the end of a game.
  *
  * @param  hand
  * @return void 
  */
-void Player::clear_usable_cards()
-{
-    this->m_usable_cards.clear();
-}
+// void Player::clear_private_cards()
+// {
+//     this->private_cards.clear();
+// }
+
+
+/**
+ * The functions clears the player's community cards, usually happends at the end of a game.
+ *
+ * @param  hand
+ * @return void 
+ */
+// void Player::clear_community_cards()
+// {
+//     this->community_cards.clear();
+// }
 
 
 /**
@@ -69,10 +72,78 @@ void Player::clear_usable_cards()
  * @param  int
  * @return void 
  */
-void Player::set_balance(int balance)
+// void Player::set_balance(int balance)
+// {
+//     this->m_balance = balance;
+// }
+
+
+inline Action get_human_player_action()
 {
-    this->m_balance = balance;
+    char _a;
+    Action action = Action::INVALID;
+    try
+    {
+        std::cin >> _a;
+        switch (_a)
+        {
+            case 'F':
+            case 'f':
+                action = Action::FOLD;
+                break;
+
+            case 'C':
+            case 'c':
+                action = Action::CHECK;
+                break;
+
+            case 'B':
+            case 'b':
+                return Action::BET;
+                break;
+
+            default:
+                throw std::runtime_error ("user entered invalid action, terminating the game\n");
+                exit (-1);
+                break;
+        }
+    }
+    catch(const std::exception& e)
+    {
+        std::cerr << e.what() << '\n';
+        exit(0);
+    }
+    return action;
 }
+
+
+inline Action get_action_based_on_strat(std::vector<float>& strat)
+{
+    // get a random float between [0, 1)
+    float r = static_cast<float> (rand()) / static_cast<float> (RAND_MAX);
+
+    int num_actions = strat.size();
+    assert(num_actions == 2 || num_actions == 3);
+    std::vector<float> cdf (num_actions, 0);
+    float sum = 0;
+    int i = 0;
+    for (; i < num_actions; i ++)
+    {
+        sum += strat[i];
+        cdf[i] = sum;
+        if (cdf[i] >= r) break;
+    }
+
+    switch (i)
+    {
+        case 0 : return Action::BET;
+        case 1 : return Action::FOLD;
+        case 2 : return Action::CHECK;
+        
+        default : return Action::INVALID;
+    }
+}
+
 
 /**
  * The function gets an action from a player, prompt the user to input an action and check if it is valid. Returns a legal action
@@ -82,70 +153,114 @@ void Player::set_balance(int balance)
  */
 Action Player::get_action(bool allow_check) const
 {
-    char _a;
     Action action = Action::INVALID;
     do
     {   
         if (allow_check)
         {
-            std::cout << "Player: " << player_name << "\n\tPlease enter F (Fold), C (Check) or B (Bet)\n";
+            debug_print("Player: %s\n\tPlease enter F (Fold), C (Check) or B (Bet)\n", player_name.c_str());
         }
         else
         {
-            std::cout << "Player: " << player_name << "\n\tPlease enter F (Fold) or B (Bet)\n";
+            debug_print("Player: %s\n\tPlease enter F (Fold) or B (Bet)\n", player_name.c_str());
         }
-        try
+
+        if (!is_cfr_bot && !is_fixed_strat_bot)
         {
-            std::cin >> _a;
-            switch (_a)
+            action = get_human_player_action();
+        }
+        else if (is_cfr_bot)
+        {
+            // use current game state to construct a infoset key
+            // but need to sort the first 3 community card and private card in ascending order to keep consistency
+            auto c_cards = this->community_cards;
+            if (!c_cards.empty())
             {
-                case 'F':
-                case 'f':
-                    action = Action::FOLD;
-                    break;
+                assert(c_cards.size() >= 3);
+                std::sort(c_cards.begin(), c_cards.begin() + 3);
+            }
+            auto p_cards = this->private_cards;
+            assert(p_cards.size() == 2);
+            std::sort(p_cards.begin(), p_cards.end());
 
-                case 'C':
-                case 'c':
-                    action = Action::CHECK;
-                    break;
+            // because action_history only stores action at the end of the previous round, there might be new actions performed in this round
+            auto up_to_date_action_history = this->action_history;
+            up_to_date_action_history.insert(up_to_date_action_history.end(), this->action_this_round.begin(), this->action_this_round.end());
 
-                case 'B':
-                case 'b':
-                    return Action::BET;
-                    break;
-
-                default:
-                    throw std::runtime_error ("user entered invalid action, terminating the game");
-                    exit (-1);
-                    break;
+            // now finally we can convert it into a infomap key and lookup
+            Infoset infoset = encode_infoset(p_cards, c_cards, up_to_date_action_history);
+            assert(this->trainer != nullptr);
+            if (trainer->infoset_map.find(infoset) != trainer->infoset_map.end())
+            {
+                debug_print("%s", "Found infoset_key\nstrat=");
+                std::vector<float> strat = trainer->infoset_map.at(infoset).sigma;
+                assert(strat.size() == 2 || strat.size() == 3);
+                if (DEBUG) UTILS::print_vec<float>(strat, std::cout);
+                action = get_action_based_on_strat(strat);
+            }
+            else
+            {
+                debug_print("%s\n", "infoset_key not found in map");
+                // std::cout << "infoset_key not found in map\n";
+                float win_rate = trainer->odds_calculator(this->private_cards, this->community_cards, this->deck, 50000);
+                assert(win_rate <= 1);
+                std::vector<float> strat;
+                if (allow_check)
+                {
+                    strat = {win_rate, 0, 1 - win_rate};
+                }
+                else
+                {
+                    strat = {win_rate, 1 - win_rate};
+                }
+                action = get_action_based_on_strat(strat);
             }
         }
-        catch(const std::exception& e)
+        else if (is_fixed_strat_bot)
         {
-            std::cerr << e.what() << '\n';
+            auto strat = allow_check ? fs.second : fs.first;
+
+            // std::cout << "Using fixed strategy\n";
+            // UTILS::print_vec<float>(strat, std::cout);
+            action = get_action_based_on_strat(strat);
+            // std::cout << "action=" << action_to_str.at(action) << std::endl;
+        }
+        else
+        {
+            fprintf(stderr, "unknown player type\n");
         }
     } while(!allow_check && action == Action::CHECK);
 
+    // assert(action != Action::INVALID);
+    debug_print(GREEN "%s\n" RESET, action_to_str.at(action).c_str());
     return action;
 }
 
 
-Hand Player::get_hand() const
-{
-    return m_hand;
-}
+// Hand Player::get_private_cards() const
+// {
+//     return this->private_cards;
+// }
 
 
-Hand Player::get_usable_cards() const
-{
-    return m_usable_cards;
-}
+// Hand Player::get_community_cards() const
+// {
+//     return this->community_cards;
+// }
 
 
-int Player::get_balance() const
-{
-    return m_balance;
-}
+// Hand get_usable_cards() const
+// {
+//     auto usable_cards = this->private_cards;
+//     usable_cards.insert(usable_cards.end(), this->community_cards.begin(), this->community_cards.end());
+//     return usable_cards;
+// }
+
+
+// int Player::get_balance() const
+// {
+//     return m_balance;
+// }
 
 /**
  * The functions record all the history action of a player by copying the round action.
@@ -153,10 +268,10 @@ int Player::get_balance() const
  * @param  none
  * @return void 
  */
-void Player::update_his_action()
-{
-    m_his_action.insert(m_his_action.end(), m_round_action.begin(), m_round_action.end());
-}
+// void Player::update_his_action()
+// {
+//     m_his_action.insert(m_his_action.end(), m_round_action.begin(), m_round_action.end());
+// }
 
 /**
  * The functions record the action of a player in the current round
@@ -165,10 +280,10 @@ void Player::update_his_action()
  * @param  Action
  * @return void 
  */
-void Player::update_round_action(Action a)
-{
-    m_round_action.push_back(a);
-}
+// void Player::update_round_action(Action a)
+// {
+//     m_round_action.push_back(a);
+// }
 
 /**
  * The functions returns the last action of a player in the current round or INVALID if the player hasn't played this round
@@ -176,15 +291,16 @@ void Player::update_round_action(Action a)
  * @param  none
  * @return void 
  */
-Action Player::get_last_action_in_round() const
-{
-    if (m_round_action.size() == 0)
-    {
-        // fprintf(stderr, "Error! Getting last action on a empty round history\n");
-        return Action::INVALID;
-    }
-    return m_round_action.back();
-}
+// Action Player::get_last_action_in_round() const
+// {
+//     if (m_round_action.size() == 0)
+//     {
+//         // fprintf(stderr, "Error! Getting last action on a empty round history\n");
+//         return Action::INVALID;
+//     }
+//     return m_round_action.back();
+// }
+
 
 /**
  * The functions sorts the hand and display the cards in a more readable form
@@ -192,22 +308,22 @@ Action Player::get_last_action_in_round() const
  * @param  none
  * @return void 
  */
-void Player::display_hand()
+void Player::display_private_cards()
 {
     // first sort the hand
-    std::sort(m_hand.begin(), m_hand.end());
+    std::sort(this->private_cards.begin(), this->private_cards.end());
     std::cout << "------------------------------------------------\n";
-    std::cout << this->player_name << " hand:\n";
-    UTILS::display_hand(m_hand);
+    std::cout << this->player_name << " Private cards:\n";
+    UTILS::display_hand(this->private_cards);
     std::cout << "\n";
 }
 
-void Player::display_usable_cards()
-{
-    std::sort(m_usable_cards.begin(), m_usable_cards.end());
-    std::cout << "------------------------------------------------\n";
-    std::cout << this->player_name << " usable cards\n";
-    UTILS::display_hand(m_usable_cards);
-    std::cout << "\n";
-}
+// void Player::display_usable_cards()
+// {
+//     this->display_private_cards();
+//     std::cout << "\n-------------------------------\n";
+//     std::cout << this->player_name << " Community cards\n";
+//     UTILS::display_hand(this->community_cards);
+//     std::cout << "\n";
+// }
 
