509796ded8de626560284b5207cfd0b5a11550d5
jerrywang1010 <40965554+jerrywang1010@users.noreply.github.com>
2022-02-17 23:27:49 -0500

    Update CFR_Trainer.cpp

--- texas_holdem/CFR_Trainer.cpp
+++ texas_holdem/CFR_Trainer.cpp
@@ -1,5 +1,75 @@
 #include "CFR_Trainer.h"
 #include <numeric>
+#include <chrono>
+#include <random>
+
+float CFR_Trainer::odds_calculator(const Hand& private_cards, const Hand& community_cards, const Hand& deck, int num_iter)
+{
+    // input: private cards, community cards, legal actions, deck size, num_iter
+    Hand pools;
+    for (size_t i = 0; i < deck.size(); i ++)
+    {
+        if (std::find(private_cards.begin(), private_cards.end(), deck[i]) != private_cards.end()
+        || std::find(community_cards.begin(), community_cards.end(), deck[i]) != community_cards.end())
+        {
+            continue;
+        }
+        pools.push_back(deck[i]);
+    }
+
+
+    int win = 0;
+    int cnt = 1;
+    int tie = 0;
+    omp::HandEvaluator m_eval = omp::HandEvaluator();
+    for (int i = 0; i < num_iter; i ++)
+    {
+        Hand hands;
+        hands.push_back(private_cards[0]);
+        hands.push_back(private_cards[1]);
+        for (size_t j = 0; j < community_cards.size(); j ++)
+        {
+            hands.push_back(community_cards[j]);
+        }
+        unsigned seed = std::chrono::system_clock::now().time_since_epoch().count();
+        std::shuffle(pools.begin(), pools.end(), std::default_random_engine(seed));
+        int hands_size = hands.size();
+        for (int k = 0; k < 9 - hands_size; k ++)
+        {
+            hands.push_back(pools[k]);
+        }
+        Hand myhands = {hands[0], hands[1], hands[2], hands[3], hands[4], hands[5], hands[6]};
+        Hand ophands = {hands[7], hands[8], hands[2], hands[3], hands[4], hands[5], hands[6]};
+        //std::cout << hands[0] << " " << hands[1] << " " << hands[2] << " " << hands[3] << " " << hands[4] << " " << hands[5] << " " << hands[6] << std::endl;
+        //std::cout << hands[7] << " " << hands[8] << " " << hands[2] << " " << hands[3] << " " << hands[4] << " " << hands[5] << " " << hands[6] << std::endl;
+        omp::Hand h = omp::Hand::empty();
+        for (Card card: myhands)
+        {
+            h += omp::Hand(card);
+        }
+        uint16_t myscore = m_eval.evaluate(h);
+        omp::Hand oph = omp::Hand::empty();
+        for (Card card: ophands)
+        {
+            oph += omp::Hand(card);
+        }
+        uint16_t opscore = m_eval.evaluate(oph);
+        if (myscore > opscore)
+        {
+            win ++;
+        }
+        if (myscore == opscore)
+        {
+            tie ++;
+        }
+        cnt ++;
+    }
+    debug_print("Odds_calculator sampling results: win=%d, tie=%d, loss=%d, total_sampling_count=%d\n", win, tie, cnt-win-tie, cnt);
+    float win_rate = win / (float)cnt;
+    debug_print("winning rate=%f\n", win_rate);
+    return win_rate;
+}
+
 
 
 // bot is player 0 by convention
@@ -32,7 +102,7 @@ float CFR_Trainer::cfr_utility_recursive(TreeNode* node, float reach_0, float re
 
     assert(this->infoset_map.find(action_node->infoset) != this->infoset_map.end());
     Infoset_value& v = this->infoset_map.at(action_node->infoset);
-    for (int i = 0; i < action_node->children.size(); i ++)
+    for (size_t i = 0; i < action_node->children.size(); i ++)
     {
         double child_reach_0=0, child_reach_1=0;
         if (action_node->infoset.active_player_idx == 0) 
@@ -56,7 +126,7 @@ float CFR_Trainer::cfr_utility_recursive(TreeNode* node, float reach_0, float re
     std::tie(cfr_reach, reach) = action_node->infoset.active_player_idx == 0 ? std::make_tuple(reach_1, reach_0) : std::make_tuple(reach_0, reach_1);
     
 
-    for (int i = 0; i < action_node->children.size(); i ++)
+    for (size_t i = 0; i < action_node->children.size(); i ++)
     {
         float action_cfr_regret = action_node->infoset.active_player_idx == 0 ? 
                                   cfr_reach * (children_state_utility[i] - utility): 
@@ -220,13 +290,28 @@ void CFR_Trainer::trainer_init()
 
 void CFR_Trainer::train(int iteration)
 {
+    int interval = iteration >= 10 ? iteration / 10 : 1;
     for (int i = 0; i < iteration; i ++)
     {
-        // std::cout << "iteration: " << i << std::endl;
-        cfr_utility_recursive(this->tree.root, 1, 1);
+        if (i % interval == 0)
+        {
+            std::cout << "iteration: " << i << std::endl;
+            float progress = (float) i / iteration;
+            int barWidth = 70;
 
+            std::cout << "[";
+            int pos = barWidth * progress;
+            for (int i = 0; i < barWidth; ++i) {
+                if (i < pos) std::cout << "=";
+                else if (i == pos) std::cout << ">";
+                else std::cout << " ";
+            }
+            std::cout << "] " << int(progress * 100.0) << " %\r";
+            std::cout.flush();
+            std::cout << std::endl;
+        }
+        cfr_utility_recursive(this->tree.root, 1, 1);
         update_sigma_recursive(this->tree.root);
-        
     }
 }
 
@@ -251,7 +336,7 @@ void CFR_Trainer::compute_nash_eq_recursive(TreeNode* node)
         std::for_each(v.cumulative_sigma.begin(), v.cumulative_sigma.end(), [&] (float n) {
             sigma_sum += n;
         });
-        for (int i = 0; i < action_node->children.size(); i ++)
+        for (size_t i = 0; i < action_node->children.size(); i ++)
         {
             v.sigma[i] = v.cumulative_sigma[i] / sigma_sum;
         }
