3c6e657a19ea04c338620b91ff96fc9d0b050acc
jerrywang1010 <40965554+jerrywang1010@users.noreply.github.com>
2022-04-09 22:35:09 -0400

    Update gametree.h

--- texas_holdem/gametree.h
+++ texas_holdem/gametree.h
@@ -11,11 +11,14 @@ private:
     omp::HandEvaluator m_eval;
 
 public:
+    std::vector<Hand> all_combo_first_round;
     std::unordered_map<int, std::vector<TreeNode*> > tree_with_depth;
     GameTree() : root(nullptr), m_eval (omp::HandEvaluator()) {}
-    void build_tree(Hand deck);
-    void recursive_build_tree(TreeNode* parent, Build_node_args args);
+    void init_all_combo_first_round(Hand deck, bool early_pruning);
+    void build_tree(Hand deck, size_t first_round_combination_start_idx, size_t interval);
+    void recursive_build_tree(TreeNode* parent, Build_node_args args, size_t first_round_combination_start_idx, size_t interval);
     void print_tree(std::ostream& s = std::cout);
     inline Node_type get_next_node_type(TreeNode* parent, Action a, int round_idx, const std::vector<Action>& action_this_round);
     inline std::vector<Action> get_legal_actions(TreeNode* parent, int round_idx, const std::vector<Action> & action_this_round);
+    void free_tree_nodes();
 };
