d58ec2fd56a6b8fc64a41111c8a2f3046c131163
jerrywang1010 <40965554+jerrywang1010@users.noreply.github.com>
2022-04-09 22:32:35 -0400

    Update CFR_Trainer.h

--- texas_holdem/CFR_Trainer.h
+++ texas_holdem/CFR_Trainer.h
@@ -8,18 +8,18 @@ typedef boost::unordered_map<Infoset, Infoset_value> InfosetMap;
 class CFR_Trainer
 {
 private:
-    GameTree& tree;
     float cfr_utility_recursive(TreeNode* node, float reach_0, float reach_1);
     inline void update_sigma_recursive(TreeNode* node);
     inline void update_sigma(ActionNode* node);
     void trainer_init_recursive(TreeNode* node);
-    void trainer_init();
     void compute_nash_eq_recursive(TreeNode* node);
 
 public:
+    GameTree* tree;
     InfosetMap infoset_map;
-    CFR_Trainer(GameTree& tree) : tree(tree) { trainer_init(); }
+    CFR_Trainer() : tree(nullptr) {};
+    void trainer_init();
     void train(int iteration = 1);
     void compute_nash_eq();
-    float odds_calculator(const Hand& private_cards, const Hand& community_cards, const Hand& deck, int num_iter);
+    void odds_calculator(const omp::HandEvaluator& m_eval, const Hand& private_cards, const Hand& community_cards, const Hand& deck, int num_iter, bool allow_check, float& b, float& c, float& f);
 };
